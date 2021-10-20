32281a67b436a6a68560ca21ac1e4b21f4fb6460
Mohammedshoaib2187 <74865302+Mohammedshoaib2187@users.noreply.github.com>
2021-10-20 10:55:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ app.use(cors({
     origin : "*"
 }))
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
 var students =[]
 
 app.post("/user-create",function(req,res){
