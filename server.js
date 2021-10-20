9034ed1dc8deb0fadb394a386caa5a4ad9277697
Mohammed Shoaib <mdshoaib093@gmail.com>
2021-10-20 09:46:56 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express=require("express");
+const app=express();
+const cors=require("cors")
+
+app.use(express.json())
+app.use(cors({
+    origin : "*"
+}))
+
+var students =[]
+
+app.post("/user-create",function(req,res){
+    req.body.id=students.length+1;
+    students.push(req.body);
+
+    res.json({
+        message : "Student added"
+    })
+})
+
+app.get("/user-list",function(req,res){
+    res.json(students)
+})
+
+app.post('/user-edit/:id', function(req, res) {
+    var id= parseInt(req.params.id)-1;
+    students[id]=req.body;
+    students[id].id=id+1
+
+    res.json({
+        message : "Student edited"
+    })
+
+});
+
+app.get('/user-edit/:id', function(req, res) {
+    var id= parseInt(req.params.id)-1;
+
+    res.json(students[id])
+
+});
+
+app.post('/user-delete/:id',function(req, res) {
+    var id= parseInt(req.params.id)-1;
+
+    if (id !== undefined) students.splice(id, 1);
+    
+    res.json({
+        message : "Student deleted"
+    })
+
+});
+
+app.listen(3000,function(){
+    console.log("Server is running")
+})
